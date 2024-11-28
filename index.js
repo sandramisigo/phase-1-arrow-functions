143c1ad3c03c90ddbd0fe8ecb6d96914897666c9
Sandra Misigo <misigosandra@gmail.com>
2024-11-28 11:10:54 +0300

    phase1arrowfunction

---
+++ index.js
@@ -0,0 +1,27 @@
+//function expression divide
+let a = 2000
+let b = 100
+
+const divide = (a, b) => {
+    return a / b
+}
+console.log(divide(a,b))
+
+
+//square arrow function
+let x = 2
+const square = (x) =>{ 
+    return x * x
+}
+
+console.log(square(x))
+
+//add arrow function
+let c = 3
+let d = 4
+const add = (c, d)=> {
+    return c+d;
+}
+console.log(add(c,d))
+
+
